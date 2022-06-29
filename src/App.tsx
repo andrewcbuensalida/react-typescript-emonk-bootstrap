a0bf5daf93f41d8193c24bf7d8f590aabaa116ad
andrewcbuensalida <andrewcbuensalida@gmail.com>
2022-06-29 12:28:00 -0700

    Implement delete button with non-functioning functions, and add user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,70 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+interface User {
+	id: number;
+	username: string;
+}
 
 function App() {
-	const [users, setUsers] = useState(["Andrew", "Kunal", "Chelsea"]);
-	const [user, setUser] = useState("");
+	const [users, setUsers] = useState<User[]>([]);
+	const [username, setUsername] = useState("");
 
-    useEffect(()=>{
+	useEffect(() => {
+		async function getUsers() {
+			try {
+				const { data } = await axios.get("");
+				console.log(`This is data in `);
+				console.log(data);
 
-    },[])
+				setUsers([
+					{ id: 1, username: "Andrew" },
+					{ id: 2, username: "Kunal" },
+					{ id: 3, username: "Chelsea" },
+				]);
+			} catch (error) {
+				console.log(`This is error:`, error);
+			}
+		}
+		getUsers();
+	}, []);
+	console.log(`This is users in `);
+	console.log(users);
+
+	async function handleAddUser() {
+		console.log(`This is in add`);
+		const id = uuidv4();
+		try {
+			const { data } = await axios.post("", { id, username });
+			console.log(`This is data in `);
+			console.log(data);
+
+			setUsers([...users]);
+		} catch (error) {
+			console.log(`This is error:`, error);
+		}
+	}
+
+	async function handleDeleteUser(id: number) {
+		console.log(`This is in delete`, id);
+		try {
+			const { data } = await axios.delete("");
+			console.log(`This is data in `);
+			console.log(data);
+
+			setUsers([
+				{ id: 1, username: "Andrew" },
+				{ id: 2, username: "Kunal" },
+				{ id: 3, username: "Chelsea" },
+			]);
+		} catch (error) {
+			console.log(`This is error:`, error);
+		}
+	}
+
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -18,13 +74,22 @@ function App() {
 					alt="logo"
 				/>
 				<p>eMonk</p>
-				<select 
-                className="h-100 p-2"
-                value={user} onChange={(e) => setUser(e.target.value)}>
+				<input
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					placeholder="Enter username"
+				/>
+				<button onClick={handleAddUser}>Add user</button>
+				<ul>
 					{users.map((user) => (
-						<option value={user}>{user}</option>
+						<>
+							<li key={user.id}>{user.username}</li>
+							<button onClick={() => handleDeleteUser(user.id)}>
+								Delete
+							</button>
+						</>
 					))}
-				</select>
+				</ul>
 			</header>
 		</div>
 	);
